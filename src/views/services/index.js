57f1ebcbf9d4920d2c929c5ec2a6600ba2145b07
Jonathan Morfin <jmorfin776@gmail.com>
2020-07-15 17:47:14 -0400

    services page content added

--- src/views/services/index.js
+++ src/views/services/index.js
@@ -37,42 +37,29 @@ class Services extends Component {
         </section>
         <section className="section">
           <div className="container">
-            <div className="row">
+            <div className="row services-sec">
               {this.state.displaySection == "Bridal" && (
                 <div className="col-md-8 col-sm-8 pull-right">
                   <p>
                     <img src={bridal} alt="" />
                   </p>
                   <h3>On Location Makeup for the Bride & Bridal Party</h3>
-                  <p>
-                    Donec laoreet malesuada varius. Aenean sit amet neque in est
-                    feugiat auctor. Vivamus condimentum consectetur sem vel
-                    tempor. Etiam et porta quam. Aliquam erat volutpat. Sed ut
-                    odio quis magna fringilla sodales a sed quam. Maecenas
-                    sollicitudin volutpat egestas. Nam quis purus ac purus
-                    tempus.
-                  </p>
+                  <ul>
+                    <li>Hair styling</li>
+                    <li>Makeup</li>
+                    <li>Artificial eyelashes</li>
+                  </ul>
                   <h3>Bridal Makeup</h3>
-                  <p>
-                    Ut in lorem consequat, elementum ligula id, cursus leo. Nam
-                    eu ex eget metus rhoncus sollicitudin vel eu nunc. Orci
-                    varius natoque penatibus et magnis dis parturient montes,
-                    nascetur ridiculus mus. Nunc mollis purus nibh, non viverra
-                    ipsum condimentum vitae. Praesent consequat, ante laoreet
-                    pulvinar cursus, urna lectus aliquam nunc, a viverra turpis
-                    ipsum rhoncus mauris. Sed a massa consequat, mattis tortor
-                    sed, tincidunt nunc. Pellentesque aliquet sem iaculis,
-                    ultrices dolor eget, consequat mauris.
-                  </p>
-                  <h3>Perfect Wedding Make Up</h3>
-                  <p>
-                    Donec laoreet malesuada varius. Aenean sit amet neque in est
-                    feugiat auctor. Vivamus condimentum consectetur sem vel
-                    tempor. Etiam et porta quam. Aliquam erat volutpat. Sed ut
-                    odio quis magna fringilla sodales a sed quam. Maecenas
-                    sollicitudin volutpat egestas. Nam quis purus ac purus
-                    tempus iaculis.
-                  </p>
+                  <ul>
+                    <li>Hair treatment</li>
+                    <li>Shampoo & style</li>
+                    <li>Custom-fit makeup</li>
+                    <li>Artificial Lashes</li>
+                  </ul>
+                  <h3>Custom Package</h3>
+                  <ul>
+                    <li>Contact for details</li>
+                  </ul>
                 </div>
               )}
               {this.state.displaySection == "TV" && (
@@ -81,35 +68,15 @@ class Services extends Component {
                     <img src={tv} alt="" />
                   </p>
                   <h3>TV Film Commercial</h3>
-                  <p>
-                    Donec laoreet malesuada varius. Aenean sit amet neque in est
-                    feugiat auctor. Vivamus condimentum consectetur sem vel
-                    tempor. Etiam et porta quam. Aliquam erat volutpat. Sed ut
-                    odio quis magna fringilla sodales a sed quam. Maecenas
-                    sollicitudin volutpat egestas. Nam quis purus ac purus
-                    tempus.
-                  </p>
-                  <h3>Bridal Makeup</h3>
-                  <p>
-                    Ut in lorem consequat, elementum ligula id, cursus leo. Nam
-                    eu ex eget metus rhoncus sollicitudin vel eu nunc. Orci
-                    varius natoque penatibus et magnis dis parturient montes,
-                    nascetur ridiculus mus. Nunc mollis purus nibh, non viverra
-                    ipsum condimentum vitae. Praesent consequat, ante laoreet
-                    pulvinar cursus, urna lectus aliquam nunc, a viverra turpis
-                    ipsum rhoncus mauris. Sed a massa consequat, mattis tortor
-                    sed, tincidunt nunc. Pellentesque aliquet sem iaculis,
-                    ultrices dolor eget, consequat mauris.
-                  </p>
-                  <h3>Perfect Wedding Make Up</h3>
-                  <p>
-                    Donec laoreet malesuada varius. Aenean sit amet neque in est
-                    feugiat auctor. Vivamus condimentum consectetur sem vel
-                    tempor. Etiam et porta quam. Aliquam erat volutpat. Sed ut
-                    odio quis magna fringilla sodales a sed quam. Maecenas
-                    sollicitudin volutpat egestas. Nam quis purus ac purus
-                    tempus iaculis.
-                  </p>
+                  <ul>
+                    <li>On site custom makeup</li>
+                    <li>Hair treatment</li>
+                    <li>Base hair color</li>
+                    <li>Partial foil or full foil</li>
+                    <li>Balayage</li>
+                    <li>Hair shampoo & style</li>
+                    <li>Extensions</li>
+                  </ul>
                 </div>
               )}
               {this.state.displaySection == "Special" && (
@@ -118,35 +85,13 @@ class Services extends Component {
                     <img src={special} alt="" />
                   </p>
                   <h3>Special occasions</h3>
-                  <p>
-                    Donec laoreet malesuada varius. Aenean sit amet neque in est
-                    feugiat auctor. Vivamus condimentum consectetur sem vel
-                    tempor. Etiam et porta quam. Aliquam erat volutpat. Sed ut
-                    odio quis magna fringilla sodales a sed quam. Maecenas
-                    sollicitudin volutpat egestas. Nam quis purus ac purus
-                    tempus.
-                  </p>
-                  <h3>Bridal Makeup</h3>
-                  <p>
-                    Ut in lorem consequat, elementum ligula id, cursus leo. Nam
-                    eu ex eget metus rhoncus sollicitudin vel eu nunc. Orci
-                    varius natoque penatibus et magnis dis parturient montes,
-                    nascetur ridiculus mus. Nunc mollis purus nibh, non viverra
-                    ipsum condimentum vitae. Praesent consequat, ante laoreet
-                    pulvinar cursus, urna lectus aliquam nunc, a viverra turpis
-                    ipsum rhoncus mauris. Sed a massa consequat, mattis tortor
-                    sed, tincidunt nunc. Pellentesque aliquet sem iaculis,
-                    ultrices dolor eget, consequat mauris.
-                  </p>
-                  <h3>Perfect Wedding Make Up</h3>
-                  <p>
-                    Donec laoreet malesuada varius. Aenean sit amet neque in est
-                    feugiat auctor. Vivamus condimentum consectetur sem vel
-                    tempor. Etiam et porta quam. Aliquam erat volutpat. Sed ut
-                    odio quis magna fringilla sodales a sed quam. Maecenas
-                    sollicitudin volutpat egestas. Nam quis purus ac purus
-                    tempus iaculis.
-                  </p>
+                  <ul>
+                    <li>In studio makeup</li>
+                    <li>Partial foil or full foil</li>
+                    <li>Hair style</li>
+                    <li>Balayage</li>
+                    <li>Extensions</li>
+                  </ul>
                 </div>
               )}
               {this.state.displaySection == "Model" && (
@@ -155,35 +100,14 @@ class Services extends Component {
                     <img src={model} alt="" />
                   </p>
                   <h3>Model Photo Shoots</h3>
-                  <p>
-                    Donec laoreet malesuada varius. Aenean sit amet neque in est
-                    feugiat auctor. Vivamus condimentum consectetur sem vel
-                    tempor. Etiam et porta quam. Aliquam erat volutpat. Sed ut
-                    odio quis magna fringilla sodales a sed quam. Maecenas
-                    sollicitudin volutpat egestas. Nam quis purus ac purus
-                    tempus.
-                  </p>
-                  <h3>Bridal Makeup</h3>
-                  <p>
-                    Ut in lorem consequat, elementum ligula id, cursus leo. Nam
-                    eu ex eget metus rhoncus sollicitudin vel eu nunc. Orci
-                    varius natoque penatibus et magnis dis parturient montes,
-                    nascetur ridiculus mus. Nunc mollis purus nibh, non viverra
-                    ipsum condimentum vitae. Praesent consequat, ante laoreet
-                    pulvinar cursus, urna lectus aliquam nunc, a viverra turpis
-                    ipsum rhoncus mauris. Sed a massa consequat, mattis tortor
-                    sed, tincidunt nunc. Pellentesque aliquet sem iaculis,
-                    ultrices dolor eget, consequat mauris.
-                  </p>
-                  <h3>Perfect Wedding Make Up</h3>
-                  <p>
-                    Donec laoreet malesuada varius. Aenean sit amet neque in est
-                    feugiat auctor. Vivamus condimentum consectetur sem vel
-                    tempor. Etiam et porta quam. Aliquam erat volutpat. Sed ut
-                    odio quis magna fringilla sodales a sed quam. Maecenas
-                    sollicitudin volutpat egestas. Nam quis purus ac purus
-                    tempus iaculis.
-                  </p>
+                  <ul>
+                    <li>On site or in studio custom makeup</li>
+                    <li>Base hair color</li>
+                    <li>Balayage</li>
+                    <li>Airbrush</li>
+                    <li>Hair shampoo & style</li>
+                    <li>Extensions</li>
+                  </ul>
                 </div>
               )}
               {this.state.displaySection == "Engagement" && (
@@ -192,35 +116,10 @@ class Services extends Component {
                     <img src={engagement} alt="" />
                   </p>
                   <h3>Engagement</h3>
-                  <p>
-                    Donec laoreet malesuada varius. Aenean sit amet neque in est
-                    feugiat auctor. Vivamus condimentum consectetur sem vel
-                    tempor. Etiam et porta quam. Aliquam erat volutpat. Sed ut
-                    odio quis magna fringilla sodales a sed quam. Maecenas
-                    sollicitudin volutpat egestas. Nam quis purus ac purus
-                    tempus.
-                  </p>
-                  <h3>Bridal Makeup</h3>
-                  <p>
-                    Ut in lorem consequat, elementum ligula id, cursus leo. Nam
-                    eu ex eget metus rhoncus sollicitudin vel eu nunc. Orci
-                    varius natoque penatibus et magnis dis parturient montes,
-                    nascetur ridiculus mus. Nunc mollis purus nibh, non viverra
-                    ipsum condimentum vitae. Praesent consequat, ante laoreet
-                    pulvinar cursus, urna lectus aliquam nunc, a viverra turpis
-                    ipsum rhoncus mauris. Sed a massa consequat, mattis tortor
-                    sed, tincidunt nunc. Pellentesque aliquet sem iaculis,
-                    ultrices dolor eget, consequat mauris.
-                  </p>
-                  <h3>Perfect Wedding Make Up</h3>
-                  <p>
-                    Donec laoreet malesuada varius. Aenean sit amet neque in est
-                    feugiat auctor. Vivamus condimentum consectetur sem vel
-                    tempor. Etiam et porta quam. Aliquam erat volutpat. Sed ut
-                    odio quis magna fringilla sodales a sed quam. Maecenas
-                    sollicitudin volutpat egestas. Nam quis purus ac purus
-                    tempus iaculis.
-                  </p>
+                  <li>Hair Styling</li>
+                  <li>In studio makeup</li>
+                  <li>Airbrush</li>
+                  <li>Skin care package</li>
                 </div>
               )}
               {this.state.displaySection == "Airbrush" && (
@@ -229,40 +128,14 @@ class Services extends Component {
                     <img src={airbrush} alt="" />
                   </p>
                   <h3>Airbrush Makeup</h3>
-                  <p>
-                    Donec laoreet malesuada varius. Aenean sit amet neque in est
-                    feugiat auctor. Vivamus condimentum consectetur sem vel
-                    tempor. Etiam et porta quam. Aliquam erat volutpat. Sed ut
-                    odio quis magna fringilla sodales a sed quam. Maecenas
-                    sollicitudin volutpat egestas. Nam quis purus ac purus
-                    tempus.
-                  </p>
-                  <h3>Bridal Makeup</h3>
-                  <p>
-                    Ut in lorem consequat, elementum ligula id, cursus leo. Nam
-                    eu ex eget metus rhoncus sollicitudin vel eu nunc. Orci
-                    varius natoque penatibus et magnis dis parturient montes,
-                    nascetur ridiculus mus. Nunc mollis purus nibh, non viverra
-                    ipsum condimentum vitae. Praesent consequat, ante laoreet
-                    pulvinar cursus, urna lectus aliquam nunc, a viverra turpis
-                    ipsum rhoncus mauris. Sed a massa consequat, mattis tortor
-                    sed, tincidunt nunc. Pellentesque aliquet sem iaculis,
-                    ultrices dolor eget, consequat mauris.
-                  </p>
-                  <h3>Perfect Wedding Make Up</h3>
-                  <p>
-                    Donec laoreet malesuada varius. Aenean sit amet neque in est
-                    feugiat auctor. Vivamus condimentum consectetur sem vel
-                    tempor. Etiam et porta quam. Aliquam erat volutpat. Sed ut
-                    odio quis magna fringilla sodales a sed quam. Maecenas
-                    sollicitudin volutpat egestas. Nam quis purus ac purus
-                    tempus iaculis.
-                  </p>
+                  <li>In studio makeup</li>
+                  <li>Full airbrush</li>
+                  <li>Skin care package</li>
                 </div>
               )}
 
               {/* col-md-8*/}
-              <div className="col-md-4 col-sm-4 pull-left holdit">
+              <div className="col-md-4 col-sm-4 pull-left sticky-bar">
                 <ul className="service-list-group">
                   <li>
                     <a
